5fc3392c895608f06849c516af8fc177d9df0b28
Tehreem Rasheed <bsf23006469@ue.edu.pk>
2026-07-17 10:03:27 +0500

    Update textilefac.py

--- textilefac.py
+++ textilefac.py
@@ -1,3 +1,6 @@
+1 - A textile factory gives a bonus if an employee produces more than 1,000 shirts in a month. The bonus is Rs. 20 per extra shirt. 
+Calculate the bonus and determine whether the employee qualifies
+
 print("Welcom to my Textile factory")
 shirts = int(input("Enter your produced shirts"))
 sh = 0
